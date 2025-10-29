d07703c45b6032340556c9b7eac9db36a1593bc8
Victor Osaikhuiwuomwan <victorosaikh@gmail.com>
2025-10-29 12:11:08 +0000

    Use Railway MySQL env vars

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ let db;
 (async () => {
   try {
     db = await mysql.createPool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-    });
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+});
     console.log("Database connected");
   } catch (err) {
     console.error("Database connection failed:", err);
